be1fd77f279cc032f22b5d81e1c34e380b016919
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-09-04 08:28:54 +0700

    CS3- Tao product,IProductDAO va ProductDAO- ProductDA0 còn thiếu khoảng 2 phuong thức

---
+++ src/main/java/com/codegym/dao/ProductDAO.java
@@ -0,0 +1,106 @@
+package com.codegym.dao;
+
+import com.codegym.model.Product;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO implements IProductDAO {
+    private String jdbcURL = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "123456";
+
+    private static final String INSERT_PRODUCT_SQL = "INSERT INTO product" + "(type, name, brand, price, image, amount) VALUES" + "(?,?,?,?,?,?);";
+    private static final String SELECT_PRODUCT_BY_ID = "select id,type,name,brand from product where id = ?";
+    private static final String SELECT_ALL_PRODUCT = "select * from product";
+    private static final String DELETE_PRODUCT_SQL =  "delete from product where id = ?;";
+    private static final String UPDATE_PRODUCT_SQL = "update product set type = ?, name = ?, brand = ?, price =?, image= ? where id= ?; ";
+
+    public ProductDAO() {
+    }
+
+    protected Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL,jdbcUsername,jdbcPassword);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    @Override
+    public void insertProduct(Product product) {
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PRODUCT_SQL)){
+            preparedStatement.setString(1, product.getType());
+            preparedStatement.setString(2, product.getName());
+            preparedStatement.setString(3, product.getBrand());
+            preparedStatement.setInt(4, product.getPrice());
+            preparedStatement.setString(5, product.getImage());
+            preparedStatement.setInt(6, product.getAmount());
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Override
+    public Product selectProduct(int id) throws SQLException {
+        Product product = null;
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_PRODUCT_BY_ID);){
+            preparedStatement.setInt(1,id);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                String id1 = rs.getString("id");
+                String type = rs.getString("type");
+                String name = rs.getString("brand");
+                String brand = rs.getString("brand");
+                product = new Product(id1,type,name,brand);
+            }
+        }
+        return product;
+    }
+
+    @Override
+    public List<Product> selectAllProducts() {
+        List<Product> product = new ArrayList<>();
+        try (
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_PRODUCT);) {
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                String id = rs.getString("id");
+                String type = rs.getString("type");
+                String name = rs.getString("name");
+                String brand = rs.getString("brand");
+                int price = rs.getInt("price");
+                String image = rs.getString("image");
+                int amount = rs.getInt("amount");
+                product.add(new Product(id,type,name,brand,price,image,amount));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return product;
+    }
+
+    @Override
+    public boolean deleteProduct(int id) throws SQLException {
+        boolean rowDeleted;
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PRODUCT_SQL);){
+            preparedStatement.setInt(1,id);
+            rowDeleted = preparedStatement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+    }
+
+    @Override
+    public boolean updateProduct(Product product) {
+        return false;
+    }
+}
---
+++ src/main/java/com/codegym/model/Product.java
@@ -0,0 +1,87 @@
+package com.codegym.model;
+
+public class Product {
+    public String id;
+    public String type;
+    public String name;
+    public String brand;
+    public int price;
+    public String image;
+    public int amount;
+
+    public Product() {
+    }
+
+    public Product(String id, String type, String name, String brand, int price, String image, int amount) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.image = image;
+        this.amount = amount;
+    }
+
+    public Product(String id, String type, String name, String brand) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.brand = brand;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
